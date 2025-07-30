04b7a8b39d4767a9d89e88eebb35072570be101d
Aryan70027 <aryanxtrader@gmail.com>
2025-07-31 01:27:50 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -115,7 +115,7 @@ export default function Home() {
         }}
         className="fixed bottom-4 right-4 text-[13px] text-black/80 pointer-events-none z-40 font-light"
       >
-        @aryanmehta27
+        Made with ❤️ by 😏
       </motion.div>
     </div>
   )
